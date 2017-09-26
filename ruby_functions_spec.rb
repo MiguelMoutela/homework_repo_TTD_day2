95078ac1f2f790630f1a3d62615c53d8207245e1
Miguel <miguelmoutela@hotmail.com>
2017-09-26 22:11:06 +0100

    added all further tests

--- ruby_functions_spec.rb
+++ ruby_functions_spec.rb
@@ -73,17 +73,36 @@ class Functions_Practice < MiniTest::Test
   #Given the length of a side of a cube calculate the volume
   def test_volume_of_cube()
     #add test code here
+    #Arrange
+      #nothing to add
+    #Act
+    volume_of_cube_result = cube(3)
+    #Assert
+    assert_equal(9, volume_of_cube_result)
   end
 
   #Given the radius of a sphere calculate the volume
   def test_volume_of_sphere()
     #add test code here
+    #Arrange
+      #nothing to add
+    #Act
+    volume_of_sphere_result() = radius(1)
+    #Assert
+    assert_equal((4/3)*Math::PI, volume_of_sphere_result)
   end
 
   #Given a value in farenheit, convert this into celsius.
   def test_fahrenheit_to_celsius()
     #add test code here
+    #Arrange
+      #nothing to add
+    #Act
+    farenheit_to_celcius_result = farenheit(32)
+    #Assert
+    assert_equal(0, farenheit_to_celcius_result)
   end
+
 
 
 end
