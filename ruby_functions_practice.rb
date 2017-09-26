e40ff9da299fba3c4d66892ee0f2853c642aa7ce
Miguel <miguelmoutela@hotmail.com>
2017-09-26 17:26:57 +0100

    fixed the name of add_string_as_number funtion

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -26,7 +26,7 @@ def join_string (string_1, string_2)
   return string_1 + string_2
 end
 
-def string_as_number(string_1, string_2)
+def add_string_as_number(string_1, string_2)
   return string_1.to_i + string_2.to_i
 end
 
